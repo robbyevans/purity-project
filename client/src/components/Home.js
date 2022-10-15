331450988ed29ebb4e11e547bb5367b5454210aa
Evans Rutto <robbyevans001@gmail.com>
2022-10-15 21:18:44 +0300

    done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,9 +6,23 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import NewEventForm from "./NewEventForm";
 import EventItem from "./EventItem";
+import ImageSlider from "./ImageSlider";
 
 
 function Home({ user }) {
+   //slider
+   const slides = [
+    { url: "https://media.istockphoto.com/photos/young-man-arms-outstretched-by-the-sea-at-sunrise-enjoying-freedom-picture-id1285301614?k=20&m=1285301614&s=612x612&w=0&h=WbwgiM4M_JWWC9ew3Mhxq1XPyfZ-Sko_RgKf7toPe7A=", title: "beach" },
+    { url: "https://www.strong4life.com/-/media/Strong4Life/Pages/Activity/Articles/6-Fun-and-Easy-Outdoor-Activities-for-Kids/SLP17_MIX_4y_Family_riding_bikes_0444.jpg", title: "boat" },
+    { url: "https://www.elitehavens.com/magazine/wp-content/uploads/2016/09/rafting-resize-1.jpg", title: "forest" },
+    { url: "https://busycontinent.com/wp-content/uploads/2022/08/image4-10.png", title: "city" },
+    { url: "https://yachtsmen.eu/wp-content/uploads/2021/07/2021_Ultra-LX_BK1_ACT-4_male.jpg", title: "italy" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
 
   // part-2
 
@@ -85,7 +99,13 @@ function Home({ user }) {
     </>;
   } else {
     return <div class="body_msg">
-      <h1>Please Login or Sign Up</h1>
+      <div className="text-msg">
+        <h1>Please Login or Sign Up</h1>
+      </div>
+      {/* //image slider */}
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>;
   }
 }
